8f3abfbdc0075dc3386c74404c4efd581f67e542
Patrick Ritter <patrick.ritter@epost.ch>
2020-09-25 09:51:20 +0200

    reformat model.py fo better readability

--- model.py
+++ model.py
@@ -10,11 +10,10 @@ import numpy as np
 import random
 from tqdm import tqdm
 from itertools import chain
-
 from skimage.transform import resize
 from skimage.io import imread, imshow
 
-#tf.enable_eager_execution()
+# tf.enable_eager_execution()
 
 IMG_WIDTH = 128
 IMG_HEIGHT = 128
@@ -50,165 +49,65 @@ y_train = Y
 inputs = tf.keras.layers.Input((IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS))
 s = tf.keras.layers.Lambda(lambda x: x / 255)(inputs)
 
-c1 = tf.keras.layers.Conv2D(
-    16,
-    (3, 3),
-    activation=tf.keras.activations.elu,
-    kernel_initializer="he_normal",
-    padding="same",
-)(s)
+args = {
+    "activation": tf.keras.activations.elu,
+    "kernel_initializer": "he_normal",
+    "padding": "same",
+}
+
+c1 = tf.keras.layers.Conv2D(16, (3, 3), **args)(s)
 c1 = tf.keras.layers.Dropout(0.1)(c1)
-c1 = tf.keras.layers.Conv2D(
-    16,
-    (3, 3),
-    activation=tf.keras.activations.elu,
-    kernel_initializer="he_normal",
-    padding="same",
-)(c1)
+c1 = tf.keras.layers.Conv2D(16, (3, 3), **args)(c1)
 p1 = tf.keras.layers.MaxPooling2D((2, 2))(c1)
 
-c2 = tf.keras.layers.Conv2D(
-    32,
-    (3, 3),
-    activation=tf.keras.activations.elu,
-    kernel_initializer="he_normal",
-    padding="same",
-)(p1)
+c2 = tf.keras.layers.Conv2D(32, (3, 3), **args)(p1)
 c2 = tf.keras.layers.Dropout(0.1)(c2)
-c2 = tf.keras.layers.Conv2D(
-    32,
-    (3, 3),
-    activation=tf.keras.activations.elu,
-    kernel_initializer="he_normal",
-    padding="same",
-)(c2)
+c2 = tf.keras.layers.Conv2D(32, (3, 3), **args)(c2)
 p2 = tf.keras.layers.MaxPooling2D((2, 2))(c2)
 
-c3 = tf.keras.layers.Conv2D(
-    64,
-    (3, 3),
-    activation=tf.keras.activations.elu,
-    kernel_initializer="he_normal",
-    padding="same",
-)(p2)
+c3 = tf.keras.layers.Conv2D(64, (3, 3), **args)(p2)
 c3 = tf.keras.layers.Dropout(0.2)(c3)
-c3 = tf.keras.layers.Conv2D(
-    64,
-    (3, 3),
-    activation=tf.keras.activations.elu,
-    kernel_initializer="he_normal",
-    padding="same",
-)(c3)
+c3 = tf.keras.layers.Conv2D(64, (3, 3), **args)(c3)
 p3 = tf.keras.layers.MaxPooling2D((2, 2))(c3)
 
-c4 = tf.keras.layers.Conv2D(
-    128,
-    (3, 3),
-    activation=tf.keras.activations.elu,
-    kernel_initializer="he_normal",
-    padding="same",
-)(p3)
+c4 = tf.keras.layers.Conv2D(128, (3, 3), **args)(p3)
 c4 = tf.keras.layers.Dropout(0.2)(c4)
-c4 = tf.keras.layers.Conv2D(
-    128,
-    (3, 3),
-    activation=tf.keras.activations.elu,
-    kernel_initializer="he_normal",
-    padding="same",
-)(c4)
+c4 = tf.keras.layers.Conv2D(128, (3, 3), **args)(c4)
 p4 = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(c4)
 
-c5 = tf.keras.layers.Conv2D(
-    256,
-    (3, 3),
-    activation=tf.keras.activations.elu,
-    kernel_initializer="he_normal",
-    padding="same",
-)(p4)
+c5 = tf.keras.layers.Conv2D(256, (3, 3), **args)(p4)
 c5 = tf.keras.layers.Dropout(0.3)(c5)
-c5 = tf.keras.layers.Conv2D(
-    256,
-    (3, 3),
-    activation=tf.keras.activations.elu,
-    kernel_initializer="he_normal",
-    padding="same",
-)(c5)
+c5 = tf.keras.layers.Conv2D(256, (3, 3), **args)(c5)
 
 u6 = tf.keras.layers.Conv2DTranspose(128, (2, 2), strides=(2, 2), padding="same")(c5)
 u6 = tf.keras.layers.concatenate([u6, c4])
-c6 = tf.keras.layers.Conv2D(
-    128,
-    (3, 3),
-    activation=tf.keras.activations.elu,
-    kernel_initializer="he_normal",
-    padding="same",
-)(u6)
+c6 = tf.keras.layers.Conv2D(128, (3, 3), **args)(u6)
 c6 = tf.keras.layers.Dropout(0.2)(c6)
-c6 = tf.keras.layers.Conv2D(
-    128,
-    (3, 3),
-    activation=tf.keras.activations.elu,
-    kernel_initializer="he_normal",
-    padding="same",
-)(c6)
+c6 = tf.keras.layers.Conv2D(128, (3, 3), **args)(c6)
 
 u7 = tf.keras.layers.Conv2DTranspose(64, (2, 2), strides=(2, 2), padding="same")(c6)
 u7 = tf.keras.layers.concatenate([u7, c3])
-c7 = tf.keras.layers.Conv2D(
-    64,
-    (3, 3),
-    activation=tf.keras.activations.elu,
-    kernel_initializer="he_normal",
-    padding="same",
-)(u7)
+c7 = tf.keras.layers.Conv2D(64, (3, 3), **args)(u7)
 c7 = tf.keras.layers.Dropout(0.2)(c7)
-c7 = tf.keras.layers.Conv2D(
-    64,
-    (3, 3),
-    activation=tf.keras.activations.elu,
-    kernel_initializer="he_normal",
-    padding="same",
-)(c7)
+c7 = tf.keras.layers.Conv2D(64, (3, 3), **args)(c7)
 
 u8 = tf.keras.layers.Conv2DTranspose(32, (2, 2), strides=(2, 2), padding="same")(c7)
 u8 = tf.keras.layers.concatenate([u8, c2])
-c8 = tf.keras.layers.Conv2D(
-    32,
-    (3, 3),
-    activation=tf.keras.activations.elu,
-    kernel_initializer="he_normal",
-    padding="same",
-)(u8)
+c8 = tf.keras.layers.Conv2D(32, (3, 3), **args)(u8)
 c8 = tf.keras.layers.Dropout(0.1)(c8)
-c8 = tf.keras.layers.Conv2D(
-    32,
-    (3, 3),
-    activation=tf.keras.activations.elu,
-    kernel_initializer="he_normal",
-    padding="same",
-)(c8)
+c8 = tf.keras.layers.Conv2D(32, (3, 3), **args)(c8)
 
 u9 = tf.keras.layers.Conv2DTranspose(16, (2, 2), strides=(2, 2), padding="same")(c8)
 u9 = tf.keras.layers.concatenate([u9, c1], axis=3)
-c9 = tf.keras.layers.Conv2D(
-    16,
-    (3, 3),
-    activation=tf.keras.activations.elu,
-    kernel_initializer="he_normal",
-    padding="same",
-)(u9)
+c9 = tf.keras.layers.Conv2D(16, (3, 3), **args)(u9)
 c9 = tf.keras.layers.Dropout(0.1)(c9)
-c9 = tf.keras.layers.Conv2D(
-    16,
-    (3, 3),
-    activation=tf.keras.activations.elu,
-    kernel_initializer="he_normal",
-    padding="same",
-)(c9)
+c9 = tf.keras.layers.Conv2D(16, (3, 3), **args)(c9)
 
 outputs = tf.keras.layers.Conv2D(1, (1, 1), activation="sigmoid")(c9)
 
 model = tf.keras.Model(inputs=[inputs], outputs=[outputs])
+
+
 checkpoint_path = "training_1/cp.ckpt"
 checkpoint_dir = os.path.dirname(checkpoint_path)
 
